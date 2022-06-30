6522e5f70fd64f8aba5b06b6dfe06842ffd05197
Joseph von Reding <joseph.vonreding@wilco.com>
2022-06-30 09:24:45 +0200

    course finished

--- 14_PThread/Threading/Main.c
+++ 14_PThread/Threading/Main.c
@@ -1,18 +1,27 @@
 #include <assert.h>
+#include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define LENGTH (10000u)
-#define NUM_RUNS (10000u)
+#define LENGTH (size_t)(10000000000u)
+#define NUM_RUNS (size_t)(10u)
+
+typedef struct
+{
+    float *array;
+    size_t length;
+} varg_thread_t;
+
 
 void serial_sum(float *arr, size_t length)
 {
     if (arr == NULL)
         return;
 
-    volatile static double sum = 0u;
+    static volatile double sum = 0u;
 
     for (size_t i = 0; i < length; i++)
     {
@@ -20,22 +29,32 @@ void serial_sum(float *arr, size_t length)
     }
 }
 
-void parallel_sum(float *arr, size_t length)
+void *parallel_sum(void *varg)
 {
-    if (arr == NULL)
-        return;
+    if (varg == NULL)
+        return NULL;
 
-    volatile static double sum = 0u;
+    varg_thread_t *varg_thread = (varg_thread_t *)varg;
 
-    for (size_t i = 0; i < length; i++)
+    static volatile double sum = 0u;
+
+    for (size_t i = 0; i < varg_thread->length; i++)
     {
-        sum += (double)arr[i];
+        sum += (double)varg_thread->array[i];
     }
+
+    return NULL;
 }
 
 int main()
 {
+
+    // init start
+
     float *arr = (float *)malloc(LENGTH * sizeof(float));
+
+    if (arr == NULL)
+        return 1;
 
     for (size_t i = 0; i < LENGTH; i++)
     {
@@ -49,29 +68,54 @@ int main()
         }
     }
 
+    // serial execution start
+
     double total_time = 0.0;
 
-    const time_t start = time(NULL);
+    time_t start = time(NULL);
     for (uint32_t run_idx = 0; run_idx < NUM_RUNS; run_idx++)
     {
         (void)serial_sum(arr, LENGTH);
     }
-    const time_t end = time(NULL);
-    total_time = (difftime(end, start) * 1000.0F) / (double)(NUM_RUNS);
+    time_t end = time(NULL);
+    total_time = (difftime(end, start) * 1000.0F);
 
-    printf("Serial - Mean execution time: %.2lf ms\n", total_time);
+    printf("Serial - Total time: %.16lf ms\n", total_time);
+    printf("Serial - Mean execution time: %.16lf ms\n", (total_time / (double)(NUM_RUNS)));
 
     total_time = 0.0;
 
-    const time_t start = time(NULL);
+    // parallel execution start
+
+    start = time(NULL);
+
+    /*
+    varg_thread_t *varg_thread = (varg_thread_t *)malloc(sizeof(varg_thread_t));
+
+    if (varg_thread == NULL)
+        return 1;
+    */
+
+    varg_thread_t varg_thread;
+
+    varg_thread.length = LENGTH;
+    varg_thread.array = arr;
+
+    pthread_t thread_id[NUM_RUNS] = {NULL};
+
     for (uint32_t run_idx = 0; run_idx < NUM_RUNS; run_idx++)
     {
-        (void)parallel_sum(arr, LENGTH);
+        pthread_create(&(thread_id[run_idx]), NULL, parallel_sum, (void *)(&varg_thread));
     }
-    const time_t end = time(NULL);
-    total_time = (difftime(end, start) * 1000.0F) / (double)(NUM_RUNS);
+    for (uint32_t run_idx = 0; run_idx < NUM_RUNS; run_idx++)
+    {
+        pthread_join(thread_id[run_idx], NULL);
+    }
+    end = time(NULL);
+    total_time = (difftime(end, start) * 1000.0F);
 
-    printf("Parall - Mean execution time: %.2lf ms\n", total_time);
+    printf("Parallel - Total time: %.16lf ms\n", total_time);
+    printf("Parallel - Mean execution time: %.16lf ms\n", (total_time / (double)(NUM_RUNS)));
 
     free(arr);
     arr = NULL;
